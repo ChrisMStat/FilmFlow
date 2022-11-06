ba7058a2b69a14abb5d30e1b2bfa3a315bfad1aa
ChrisMStat <98663137+ChrisMStat@users.noreply.github.com>
2022-11-06 15:52:27 -0600

    login, register, about us, and routing setup

---
+++ src/Login.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react"
+
+export default function (props) {
+    let [loginOption, setOption] = useState("signin")
+
+    const changeAuthMode = () => {
+        setOption(loginOption === "signin" ? "signup" : "signin")
+    }
+
+    if (loginOption === "signin") {
+        return (
+            <div className="Login-form-container">
+                <form className="Login-form">
+                    <div className="Login-form-content">
+                        <h3 className="Login-form-title">Sign In</h3>
+                        <div className="text-center">
+                            New User?{" "}
+                            <span className="link-primary" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Email address</label>
+                            <input
+                                type="email"
+                                className="form-control mt-1"
+                                placeholder="Enter email"
+                            />
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control mt-1"
+                                placeholder="Enter password"
+                            />
+                        </div>
+                        <div className="d-grid gap-2 mt-3">
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
+                        </div>
+                        <p className="text-center mt-2">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    return (
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign In</h3>
+                    <div className="text-center">
+                        Already registered?{" "}
+                        <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Full Name</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="e.g Jane Doe"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Email Address"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="text-center mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    )
+}
